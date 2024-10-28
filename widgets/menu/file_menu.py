44b2cc9c17e056c1a0ab454a4fdb51dc4a9d71f2
Yoshi2 <renoly2@users.noreply.github.com>
2024-10-28 17:29:17 +0100

    store current file path, run plugin events before/after load, wrap saving in try:except and don't render during saving

--- widgets/menu/file_menu.py
+++ widgets/menu/file_menu.py
@@ -54,6 +54,7 @@ class EditorFileMenu(QMenu):
 
         self.last_chosen_type = None
         self.level_paths = None
+        self.current_path = None
 
         save_file_shortcut = QShortcut(Qt.Key.Key_Control + Qt.Key.Key_S, self)
         save_file_shortcut.activated.connect(self.button_save_level)
@@ -106,6 +107,8 @@ class EditorFileMenu(QMenu):
                 func_open = gzip.open
             else:
                 func_open = open
+
+            self.editor.menubar.plugin_menu.execute_event("before_load")
 
             with func_open(filepath, "rb") as f:
                 try:
@@ -192,6 +195,7 @@ class EditorFileMenu(QMenu):
                     QApplication.processEvents()
 
                     self.editor.pathsconfig["xml"] = filepath
+                    self.current_path = filepath
                     self.editor.setCursor(QtGui.QCursor(Qt.CursorShape.ArrowCursor))
                     cursor = QtGui.QCursor()
                     cursor.setShape(Qt.CursorShape.ArrowCursor)
@@ -215,6 +219,9 @@ class EditorFileMenu(QMenu):
 
                     QApplication.processEvents()
                     self.editor.level_view.start_redrawing()
+
+                    self.editor.menubar.plugin_menu.execute_event("after_load")
+
                 except Exception as error:
                     self.editor.level_view.start_redrawing()
                     self.is_loading = False
@@ -225,132 +232,138 @@ class EditorFileMenu(QMenu):
 
     @catch_exception_with_dialog
     def button_save_level(self, *args, **kwargs):
-        if self.level_paths is not None:
-            levelpaths = self.level_paths
-            progressbar = LoadingProgress("Saving")
-            progressbar.progressupdate.connect(self.updatestatus)
+        try:
+            self.editor.level_view.stop_redrawing()
+            if self.level_paths is not None:
+                levelpaths = self.level_paths
+                progressbar = LoadingProgress("Saving")
+                progressbar.progressupdate.connect(self.updatestatus)
 
-            base = os.path.dirname(self.current_gen_path)
-            fname = os.path.basename(self.current_gen_path)
-            progressbar.set(0)
+                base = os.path.dirname(self.current_gen_path)
+                fname = os.path.basename(self.current_gen_path)
+                progressbar.set(0)
 
-            for id, window in self.editor.pik_control.edit_windows.items():
-                try:
-                    self.editor.pik_control.update_editwindow_position(id)
-                    self.editor.pik_control.save_object_data(id, mass_save=True)
-                except Exception as err:
-                    self.editor.pik_control.activate_window(id)
-                    open_error_dialog("Error while saving object {0}: \n{1}\nFix the error or close the object's edit window, then try saving again.".format(id, str(err)), None)
-                    self.editor.level_view.do_redraw(force=True)
-                    self.editor.leveldatatreeview.updatenames()
-                    return
-
-
-            self.editor.level_view.do_redraw(force=True)
-            self.editor.leveldatatreeview.updatenames()
-
-            progressbar.set(5)
-            for object in self.level_data.objects.values():
-                object.update_xml()
-            self.editor.level_view.selected_positions = []
-            for obj in self.editor.level_view.selected:
-                if obj.getmatrix() is not None:
-                    self.editor.level_view.selected_positions.append(obj.getmatrix())
-            progressbar.set(10)
-
-            if self.editor.editorconfig.getboolean("regenerate_pf2", fallback=False):
-                regenerate_waypoints = self.editor.editorconfig.getboolean("regenerate_waypoints", fallback=False)
-                try:
-                    pf2path = os.path.join(base, fname.replace("xml", "pf2"))
-                    pf2 = PF2(pf2path)
-                except FileNotFoundError:
-                    pass
-                else:
-                    pf2.update_boundary(self.level_data,
-                                        os.path.join(base, fname.replace(".xml", "")),
-                                        self.editor.level_view.bwterrain,
-                                        self.editor.level_view.waterheight,
-                                        regenerate_waypoints)
-                    pf2.save(pf2path)
-            else:
-                print("Skipping PF2..")
-
-            progressbar.set(20)
-            if (self.editor.editorconfig.getboolean("recompile_lua", fallback=True)
-                and self.editor.lua_workbench.is_initialized()):
-                try:
-                    respath = os.path.join(base, levelpaths.resourcepath)
-                    self.editor.lua_workbench.repack_scripts(respath,
-                                                             respath,
-                                                             scripts=[x.mName for x in self.level_data.scripts.values()]
-                                                             )
-                except Exception as err:
-                    msgbox = QtWidgets.QMessageBox()
-                    msgbox.setText(
-                        "An error appeared during compilation:\n\n"+str(err))
-                    msgbox.setInformativeText("Do you want to continue saving? This will skip compilation of Lua scripts.")
-                    msgbox.setStandardButtons(
-                        QtWidgets.QMessageBox.StandardButton.Yes | QtWidgets.QMessageBox.StandardButton.No)
-                    #msgbox.setDefaultButton(QtWidgets.QMessageBox.StandardButton.No)
-                    msgbox.setIcon(QtWidgets.QMessageBox.Icon.Warning)
-                    msgbox.setWindowIcon(QtGui.QIcon('resources/icon.ico'))
-                    msgbox.setWindowTitle("Warning")
-                    result = msgbox.exec()
-
-                    if result == QtWidgets.QMessageBox.StandardButton.No:
+                for id, window in self.editor.pik_control.edit_windows.items():
+                    try:
+                        self.editor.pik_control.update_editwindow_position(id)
+                        self.editor.pik_control.save_object_data(id, mass_save=True)
+                    except Exception as err:
+                        self.editor.pik_control.activate_window(id)
+                        open_error_dialog("Error while saving object {0}: \n{1}\nFix the error or close the object's edit window, then try saving again.".format(id, str(err)), None)
+                        self.editor.level_view.do_redraw(force=True)
+                        self.editor.leveldatatreeview.updatenames()
                         return
 
-            progressbar.set(25)
-            for object in self.preload_data.objects.values():
-                object.update_xml()
-            progressbar.set(30)
 
-            tmp = BytesIO()
-            print("Writing level data to temp file...")
-            self.level_data.write(tmp)
-            progressbar.set(70)
-            tmp2 = BytesIO()
+                self.editor.level_view.do_redraw(force=True)
+                self.editor.leveldatatreeview.updatenames()
 
-            print("Writing preload data to temp file...")
-            self.preload_data.write(tmp2)
+                progressbar.set(5)
+                for object in self.level_data.objects.values():
+                    object.update_xml()
+                self.editor.level_view.selected_positions = []
+                for obj in self.editor.level_view.selected:
+                    if obj.getmatrix() is not None:
+                        self.editor.level_view.selected_positions.append(obj.getmatrix())
+                progressbar.set(10)
 
-            if levelpaths.objectpath.endswith(".gz"):
-                with gzip.open(os.path.join(base, levelpaths.objectpath), "wb") as g:
-                    g.write(tmp.getvalue())
-                """if self.level_paths.objectfilepadding is not None:
-                    with open(os.path.join(base, levelpaths.objectpath), "r+b") as g:
-                        g.seek(0, 2)
-                        if g.tell() < self.level_paths.objectfilepadding:
-                            g.write(b"\x00"*(self.level_paths.objectfilepadding - g.tell()))"""
-            else:
-                with open(os.path.join(base, levelpaths.objectpath), "wb") as g:
-                    g.write(tmp.getvalue())
-                    if self.level_paths.objectfilepadding is not None:
-                        if g.tell() < self.level_paths.objectfilepadding:
-                            g.write(b" "*(self.level_paths.objectfilepadding - g.tell()))
+                if self.editor.editorconfig.getboolean("regenerate_pf2", fallback=False):
+                    regenerate_waypoints = self.editor.editorconfig.getboolean("regenerate_waypoints", fallback=False)
+                    try:
+                        pf2path = os.path.join(base, fname.replace("xml", "pf2"))
+                        pf2 = PF2(pf2path)
+                    except FileNotFoundError:
+                        pass
+                    else:
+                        pf2.update_boundary(self.level_data,
+                                            os.path.join(base, fname.replace(".xml", "")),
+                                            self.editor.level_view.bwterrain,
+                                            self.editor.level_view.waterheight,
+                                            regenerate_waypoints)
+                        pf2.save(pf2path)
+                else:
+                    print("Skipping PF2..")
 
-            progressbar.set(90)
+                progressbar.set(20)
+                if (self.editor.editorconfig.getboolean("recompile_lua", fallback=True)
+                    and self.editor.lua_workbench.is_initialized()):
+                    try:
+                        respath = os.path.join(base, levelpaths.resourcepath)
+                        self.editor.lua_workbench.repack_scripts(respath,
+                                                                 respath,
+                                                                 scripts=[x.mName for x in self.level_data.scripts.values()]
+                                                                 )
+                    except Exception as err:
+                        msgbox = QtWidgets.QMessageBox()
+                        msgbox.setText(
+                            "An error appeared during compilation:\n\n"+str(err))
+                        msgbox.setInformativeText("Do you want to continue saving? This will skip compilation of Lua scripts.")
+                        msgbox.setStandardButtons(
+                            QtWidgets.QMessageBox.StandardButton.Yes | QtWidgets.QMessageBox.StandardButton.No)
+                        #msgbox.setDefaultButton(QtWidgets.QMessageBox.StandardButton.No)
+                        msgbox.setIcon(QtWidgets.QMessageBox.Icon.Warning)
+                        msgbox.setWindowIcon(QtGui.QIcon('resources/icon.ico'))
+                        msgbox.setWindowTitle("Warning")
+                        result = msgbox.exec()
 
-            if levelpaths.preloadpath.endswith(".gz"):
-                with gzip.open(os.path.join(base, levelpaths.preloadpath), "wb") as g:
-                    g.write(tmp2.getvalue())
-                """if self.level_paths.preloadpadding is not None:
-                    with open(os.path.join(base, levelpaths.preloadpath), "ab") as g:
-                        g.seek(0, 2)
-                        if g.tell() < self.level_paths.preloadpadding:
-                            g.write(b"\x00"*(self.level_paths.preloadpadding - g.tell()))"""
-            else:
-                with open(os.path.join(base, levelpaths.preloadpath), "wb") as g:
-                    g.write(tmp2.getvalue())
-                    if self.level_paths.preloadpadding is not None:
-                        if g.tell() < self.level_paths.preloadpadding:
-                            g.write(b" "*(self.level_paths.preloadpadding - g.tell()))
+                        if result == QtWidgets.QMessageBox.StandardButton.No:
+                            return
 
-            print("Done!")
-            progressbar.set(100)
-            self.editor.set_has_unsaved_changes(False)
-        #else:
-        #    self.button_save_level_as()
+                progressbar.set(25)
+                for object in self.preload_data.objects.values():
+                    object.update_xml()
+                progressbar.set(30)
+
+                tmp = BytesIO()
+                print("Writing level data to temp file...")
+                self.level_data.write(tmp)
+                progressbar.set(70)
+                tmp2 = BytesIO()
+
+                print("Writing preload data to temp file...")
+                self.preload_data.write(tmp2)
+
+                if levelpaths.objectpath.endswith(".gz"):
+                    with gzip.open(os.path.join(base, levelpaths.objectpath), "wb") as g:
+                        g.write(tmp.getvalue())
+                    """if self.level_paths.objectfilepadding is not None:
+                        with open(os.path.join(base, levelpaths.objectpath), "r+b") as g:
+                            g.seek(0, 2)
+                            if g.tell() < self.level_paths.objectfilepadding:
+                                g.write(b"\x00"*(self.level_paths.objectfilepadding - g.tell()))"""
+                else:
+                    with open(os.path.join(base, levelpaths.objectpath), "wb") as g:
+                        g.write(tmp.getvalue())
+                        if self.level_paths.objectfilepadding is not None:
+                            if g.tell() < self.level_paths.objectfilepadding:
+                                g.write(b" "*(self.level_paths.objectfilepadding - g.tell()))
+
+                progressbar.set(90)
+
+                if levelpaths.preloadpath.endswith(".gz"):
+                    with gzip.open(os.path.join(base, levelpaths.preloadpath), "wb") as g:
+                        g.write(tmp2.getvalue())
+                    """if self.level_paths.preloadpadding is not None:
+                        with open(os.path.join(base, levelpaths.preloadpath), "ab") as g:
+                            g.seek(0, 2)
+                            if g.tell() < self.level_paths.preloadpadding:
+                                g.write(b"\x00"*(self.level_paths.preloadpadding - g.tell()))"""
+                else:
+                    with open(os.path.join(base, levelpaths.preloadpath), "wb") as g:
+                        g.write(tmp2.getvalue())
+                        if self.level_paths.preloadpadding is not None:
+                            if g.tell() < self.level_paths.preloadpadding:
+                                g.write(b" "*(self.level_paths.preloadpadding - g.tell()))
+
+                print("Done!")
+                progressbar.set(100)
+                self.editor.level_view.start_redrawing()
+                self.editor.set_has_unsaved_changes(False)
+            #else:
+            #    self.button_save_level_as()
+        except Exception as err:
+            self.editor.level_view.start_redrawing()
+            raise
 
     def button_save_level_as(self, *args, **kwargs):
         self._button_save_level_as(True, *args, **kwargs)
