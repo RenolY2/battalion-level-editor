0289c2ba9ea138f106026c58a273ea487fb7d911
Yoshi2 <renoly2@users.noreply.github.com>
2025-04-23 09:29:51 +0200

    Fix crash when running editor without existing config

--- widgets/filter_view.py
+++ widgets/filter_view.py
@@ -344,7 +344,11 @@ class FilterViewMenu(NonAutodismissibleMenu):
                 if toggle.option3d_exists:
                     toggle.action_select_toggle.setChecked(visible3d)
 
-        full_scenery = cfg["View Filter Toggles"].getboolean("full_scenery", fallback=False)
+        if "View Filter Toggles" in cfg:
+            full_scenery = cfg["View Filter Toggles"].getboolean("full_scenery", fallback=False)
+        else:
+            full_scenery = False
+
         self.show_full_scenery_action.setChecked(full_scenery)
 
     def save(self, cfg):
